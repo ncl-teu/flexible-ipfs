66d70b1674ef017d5ac29ae94f06e0ee5417477e
kanemih <kanemitsuh@stf.teu.ac.jp>
2023-11-15 01:47:43 +0900

    Add files via upload

---
+++ src/main/java/org/peergos/HostBuilder.java
@@ -0,0 +1,191 @@
+package org.peergos;
+
+import identify.pb.*;
+import io.ipfs.multiaddr.*;
+import io.ipfs.multihash.Multihash;
+import io.libp2p.core.*;
+import io.libp2p.core.crypto.*;
+import io.libp2p.core.dsl.*;
+import io.libp2p.core.multiformats.*;
+import io.libp2p.core.multistream.*;
+import io.libp2p.core.mux.*;
+import io.libp2p.crypto.keys.*;
+import io.libp2p.etc.types.*;
+import io.libp2p.protocol.*;
+import io.libp2p.security.noise.*;
+import io.libp2p.security.tls.*;
+import io.libp2p.transport.tcp.*;
+import io.libp2p.core.crypto.KeyKt;
+import org.ncl.kadrtt.core.Kad;
+import org.peergos.blockstore.*;
+import org.peergos.protocol.autonat.*;
+import org.peergos.protocol.bitswap.*;
+import org.peergos.protocol.circuit.*;
+import org.peergos.protocol.dht.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class HostBuilder {
+    private PrivKey privKey;
+    private PeerId peerId;
+    private List<String> listenAddrs = new ArrayList<>();
+    private List<ProtocolBinding> protocols = new ArrayList<>();
+    private List<StreamMuxerProtocol> muxers = new ArrayList<>();
+
+    public HostBuilder() {
+    }
+
+    public PrivKey getPrivateKey() {
+        return privKey;
+    }
+
+    public PeerId getPeerId() {
+        return peerId;
+    }
+
+    public List<ProtocolBinding> getProtocols() {
+        return this.protocols;
+    }
+
+    public Optional<Kademlia> getWanDht() {
+        return protocols.stream()
+                .filter(p -> p instanceof Kademlia && p.getProtocolDescriptor().getAnnounceProtocols().contains("/ipfs/kad/1.0.0"))
+                .map(p -> (Kademlia)p)
+                .findFirst();
+    }
+
+    public Optional<Bitswap> getBitswap() {
+        return protocols.stream()
+                .filter(p -> p instanceof Bitswap)
+                .map(p -> (Bitswap)p)
+                .findFirst();
+    }
+
+    public Optional<CircuitHopProtocol.Binding> getRelayHop() {
+        return protocols.stream()
+                .filter(p -> p instanceof CircuitHopProtocol.Binding)
+                .map(p -> (CircuitHopProtocol.Binding)p)
+                .findFirst();
+    }
+
+    public HostBuilder addMuxers(List<StreamMuxerProtocol> muxers) {
+        this.muxers.addAll(muxers);
+        return this;
+    }
+
+    public HostBuilder addProtocols(List<ProtocolBinding> protocols) {
+        this.protocols.addAll(protocols);
+        return this;
+    }
+
+    public HostBuilder addProtocol(ProtocolBinding protocols) {
+        this.protocols.add(protocols);
+        return this;
+    }
+
+    public HostBuilder listen(List<MultiAddress> listenAddrs) {
+        this.listenAddrs.addAll(listenAddrs.stream().map(MultiAddress::toString).collect(Collectors.toList()));
+        return this;
+    }
+
+    public HostBuilder generateIdentity() {
+        return setPrivKey(Ed25519Kt.generateEd25519KeyPair().getFirst());
+    }
+
+    public HostBuilder setIdentity(byte[] privKey) {
+        return setPrivKey(KeyKt.unmarshalPrivateKey(privKey));
+    }
+
+
+
+    public HostBuilder setPrivKey(PrivKey privKey) {
+        this.privKey = privKey;
+        this.peerId = PeerId.fromPubKey(privKey.publicKey());
+        return this;
+    }
+
+    public static HostBuilder create(int listenPort,
+                                     ProviderStore providers,
+                                     RecordStore records,
+                                     Blockstore blocks,
+                                     BlockRequestAuthoriser authoriser) {
+        HostBuilder builder = new HostBuilder()
+                .generateIdentity()
+                .listen(List.of(new MultiAddress("/ip4/0.0.0.0/tcp/" + listenPort)));
+        Multihash ourPeerId = Multihash.deserialize(builder.peerId.getBytes());
+        Kademlia dht = new Kademlia(new KademliaEngine(ourPeerId, providers, records), false);
+        Kad.getIns().setKadDHT(dht);
+        CircuitStopProtocol.Binding stop = new CircuitStopProtocol.Binding();
+        CircuitHopProtocol.RelayManager relayManager = CircuitHopProtocol.RelayManager.limitTo(builder.privKey, ourPeerId, 5);
+        return builder.addProtocols(List.of(
+                new Ping(),
+                new AutonatProtocol.Binding(),
+                new CircuitHopProtocol.Binding(relayManager, stop),
+                new Bitswap(new BitswapEngine(blocks, authoriser)),
+                dht));
+    }
+
+    public static Host build(int listenPort,
+                             List<ProtocolBinding> protocols) {
+        return new HostBuilder()
+                .generateIdentity()
+                .listen(List.of(new MultiAddress("/ip4/0.0.0.0/tcp/" + listenPort)))
+                .addProtocols(protocols)
+                .build();
+    }
+
+    public Host build() {
+        if (muxers.isEmpty())
+            muxers.addAll(List.of(StreamMuxerProtocol.getYamux(), StreamMuxerProtocol.getMplex()));
+        return build(privKey, listenAddrs, protocols, muxers);
+    }
+
+    public static Host build(PrivKey privKey,
+                             List<String> listenAddrs,
+                             List<ProtocolBinding> protocols,
+                             List<StreamMuxerProtocol> muxers) {
+        Host host = BuilderJKt.hostJ(Builder.Defaults.None, b -> {
+            b.getIdentity().setFactory(() -> privKey);
+            b.getTransports().add(TcpTransport::new);
+            b.getSecureChannels().add((k, m) -> new NoiseXXSecureChannel(k, m));
+            b.getSecureChannels().add((k, m) -> new TlsSecureChannel(k, m));
+            b.getMuxers().addAll(muxers);
+            b.getAddressBook().setImpl(new RamAddressBook());
+            // Uncomment to add mux debug logging
+//            b.getDebug().getMuxFramesHandler().addLogger(LogLevel.INFO, "MUX");
+
+            for (ProtocolBinding<?> protocol : protocols) {
+                b.getProtocols().add(protocol);
+                if (protocol instanceof AddressBookConsumer)
+                    ((AddressBookConsumer) protocol).setAddressBook(b.getAddressBook().getImpl());
+            }
+
+            IdentifyOuterClass.Identify.Builder identifyBuilder = IdentifyOuterClass.Identify.newBuilder()
+                    .setProtocolVersion("ipfs/0.1.0")
+                    .setAgentVersion("nabu/v0.1.0")
+                    .setPublicKey(ByteArrayExtKt.toProtobuf(privKey.publicKey().bytes()))
+                    .addAllListenAddrs(listenAddrs.stream()
+                            .map(Multiaddr::fromString)
+                            .map(Multiaddr::serialize)
+                            .map(ByteArrayExtKt::toProtobuf)
+                            .collect(Collectors.toList()));
+            for (ProtocolBinding<?> protocol : protocols) {
+                identifyBuilder = identifyBuilder.addAllProtocols(protocol.getProtocolDescriptor().getAnnounceProtocols());
+            }
+            b.getProtocols().add(new Identify(identifyBuilder.build()));
+
+            for (String listenAddr : listenAddrs) {
+                b.getNetwork().listen(listenAddr);
+            }
+
+            b.getConnectionHandlers().add(conn -> System.out.println(conn.localAddress() +
+                    " received connection from " + conn.remoteAddress() +
+                    " on transport " + conn.transport()));
+        });
+        for (ProtocolBinding protocol : protocols) {
+            if (protocol instanceof HostConsumer)
+                ((HostConsumer)protocol).setHost(host);
+        }
+        return host;
+    }
+}
---
+++ src/main/java/org/peergos/PeerAddresses.java
@@ -0,0 +1,60 @@
+package org.peergos;
+
+import com.google.protobuf.*;
+import io.ipfs.cid.*;
+import io.ipfs.multiaddr.*;
+import io.ipfs.multihash.*;
+import io.libp2p.core.*;
+import org.peergos.protocol.dht.pb.*;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class PeerAddresses implements Serializable{
+    public final Multihash peerId;
+    public List<MultiAddress> addresses;
+
+    public PeerAddresses(Multihash peerId, List<MultiAddress> addresses) {
+        this.peerId = peerId;
+        this.addresses = addresses;
+    }
+
+    public List<MultiAddress> getPublicAddresses() {
+        return addresses.stream().filter(a -> a.isPublic(false)).collect(Collectors.toList());
+    }
+
+
+
+    public static PeerAddresses fromProtobuf(Dht.Message.Peer peer) {
+        Multihash peerId = Multihash.deserialize(peer.getId().toByteArray());
+        List<MultiAddress> addrs = peer.getAddrsList()
+                .stream()
+                .map(b -> new MultiAddress(b.toByteArray()))
+                .collect(Collectors.toList());
+        return new PeerAddresses(peerId, addrs);
+    }
+
+    public Dht.Message.Peer toProtobuf() {
+        return Dht.Message.Peer.newBuilder()
+                .setId(ByteString.copyFrom(peerId.toBytes()))
+                .addAllAddrs(addresses.stream()
+                        .map(a -> ByteString.copyFrom(a.getBytes()))
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    public static PeerAddresses fromHost(Host host) {
+        Multihash peerId = Multihash.deserialize(host.getPeerId().getBytes());
+        List<MultiAddress> addrs = host.listenAddresses()
+                .stream()
+                .map(b -> new MultiAddress(b.serialize()))
+                .collect(Collectors.toList());
+        return new PeerAddresses(peerId, addrs);
+    }
+
+    @Override
+    public String toString() {
+        return peerId + ": " + addresses;
+    }
+}
