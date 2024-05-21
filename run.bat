0217489d9b5aaac9670f539e4164c5e285e016aa
kanemih <kanemih@waseda.jp>
2024-05-22 07:57:34 +0900

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 set HOME=.
 set IPFS_HOME=.ipfs
-java -cp .;classes/production/nabu-master;lib/ipfslib.jar;lib/jackson-core-2.15.2.jar;lib/jackson-databind-2.15.2.jar;lib/commons-math-2.0.jar org.peergos.APIServer Addresses.API /ip4/127.0.0.1/tcp/5001
+java -cp .;lib/ipfs-ncl.jar;lib/ipfslib.jar;lib/jackson-core-2.15.2.jar;lib/jackson-databind-2.15.2.jar;lib/commons-math-2.0.jar org.peergos.APIServer Addresses.API /ip4/127.0.0.1/tcp/5001
 pause;
